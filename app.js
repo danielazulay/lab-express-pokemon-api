ac2bad7d27815d8483f8adb85d03643c12c3be42
Daniel Azulay <you@example.comdanielazulay20@gmail.com>
2021-07-12 22:28:22 -0300

    firts

--- app.js
+++ app.js
@@ -10,3 +10,56 @@ const app = express();
 // -- Define your route listeners here! --
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
+
+
+app.use(express.json());
+
+app.get('/all',(req,res)=>{
+
+    return res.json(allPokemon);
+})
+
+
+
+app.get('/all/:id',(req,res)=>{
+
+    const id = req.params.id;
+
+  
+
+        const resposta =  allPokemon.find((poke)=>{
+          return poke.id.toString()===id
+        })
+        if(resposta){
+            return res.json(resposta)
+        }else{
+            return res.json({ msg: "Contact not found." });
+        }
+    
+   
+})
+
+
+app.get('/all/search',(req,res)=>{
+
+    const queryName = req.query
+    
+console.log(queryName)
+  
+
+        const resposta =  allPokemon.find((poke)=>{
+
+              return poke.name===queryName
+         
+        })
+
+        if(resposta){
+            return res.json(resposta)
+        }else{
+            return res.json({ msg: "Contact not found." });
+        }
+    
+        res.json(queryName);
+    
+   
+})
